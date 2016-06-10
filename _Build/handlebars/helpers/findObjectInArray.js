3de0778fca29ecc432cb1d36a4009a0e89938be1
Mike Mellor <mike.mellor@f-grp.com>
2016-06-10 22:14:21 +0100

    adding content

--- _Build/handlebars/helpers/findObjectInArray.js
+++ _Build/handlebars/helpers/findObjectInArray.js
@@ -1,6 +1,9 @@
 module.exports = function (json) {
 	for(var i = 0, len = json.hash.in.length; i < len; i++){
-		if(json.hash.in[i][json.hash.key] === json.hash.value){
+		if(json.hash.in[i][json.hash.key] === json.hash.object){
+			if(json.hash.getValue){
+				return json.hash.in[i][json.hash.getValue];	
+			}
 			return json.hash.in[i];
 		}
 	}
