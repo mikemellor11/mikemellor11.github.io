aa7345a18f29a8a871572870c75e3ccb9687606b
Mike Mellor <mike.mellor@f-grp.com>
2016-06-10 18:43:04 +0100

    homepage content

--- _Build/js/!!--script.js
+++ _Build/js/!!--script.js
@@ -3,8 +3,7 @@
 
     FastClick.attach(document.body);
 
-    document.querySelector('.js-chartSelect').onchange = function(){
-    	chartManager.chartIndex = this.selectedIndex;
-    	chartManager.update();
-    };
+    baseJS();
+
+    pageJS();
 })();
