c5a20c587ff4bcd3bee61828695b776a35a66108
Mike Mellor <mikemellor11@hotmail.com>
2017-01-02 16:38:50 +0000

    added username / name to travis

---
+++ .travis/push.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd _Output && git add --all && git commit --allow-empty -m 'Deployed' && git reset --soft HEAD~2 && git commit -m 'Deployed'
+
+git remote add origin-pages https://${token}@github.com/mikemellor11/mikemellor11.github.io.git > /dev/null 2>&1
+
+git push --quiet --set-upstream origin-pages master -f
