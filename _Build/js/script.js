3e276b2e6895c1df26a6b57feb003c0100da1a8b
mikemellor11 <mikemellor11@hotmail.com>
2019-02-23 22:17:11 +0000

    styling / weight chart

--- _Build/js/script.js
+++ _Build/js/script.js
@@ -6,8 +6,26 @@ import dayjs from "dayjs";
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 dayjs.extend(customParseFormat);
 
+import { Line } from "@fishawack/lab-d3";
+
+import { timeParse } from "d3-time-format";
+
 (() => {
 	if(navigator.userAgent === 'jsdom'){ return; }
+
+	var chart = new Line('.chart--weight')
+		.init()
+		.att({
+			margin: {
+				bottom: 40,
+				left: 40
+			},
+			scale: {
+				x: "date"
+			},
+			parseDate: timeParse('%d/%m/%Y'),
+		})
+		.render();
 
 	Utility.load("media/data/weight.json", (response) => {
 		var data = JSON.parse(response);
@@ -20,8 +38,24 @@ dayjs.extend(customParseFormat);
 		Utility.eachNode('.js-lastWorkout', (node) => {
 			node.textContent = dayjs().diff(dayjs(latest.date, "DD/MM/YYYY"), 'days');
 		});
+
+		chart.data([
+				{
+					values: data.map(function(d){
+						return {
+							key: d.date,
+							value: d.weight
+						};
+					})
+				}
+			])
+			.render();
 	});
 
+	window.onresize = function() {
+		chart.resize()
+			.renderSync();
+	};
 
 	document.querySelector('html').classList.remove('loading');
 	document.querySelector('html').classList.add('loaded');
