d125f212b83000b9f2d8e1f5ac6102e513faecc0
mikemellor11 <mikemellor11@hotmail.com>
2020-01-12 22:06:21 +0000

    diet moved to stand alone page

---
+++ _Build/js/--diet.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import Vue from "vue";
+import store from "./libs/store";
+
+import * as Utility from "./libs/utility.js";
+
+(() => {
+	if(navigator.userAgent === 'jsdom'){ return; }
+
+	window.socket = require("socket.io-client")(`http://${window.location.hostname}:8888`);
+
+	var food = new Vue({el: '#food', store, render: h => h(require('../vue/components/VFood/VFood.vue').default)});
+	var shopping = new Vue({el: '#shopping', store, render: h => h(require('../vue/components/VShopping/VShopping.vue').default)});
+	var macros = new Vue({el: '#macros', store, render: h => h(require('../vue/components/VMacros/VMacros.vue').default)});
+
+	Utility.load("media/data/food.json").then(d => store.commit('foods', d));
+	Utility.load("media/data/macros.json").then(d => store.commit('macros', d));
+})();
