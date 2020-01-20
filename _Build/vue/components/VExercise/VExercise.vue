c3c65c024636d678934bd39ebef0356905a1058e
mikemellor11 <mikemellor11@hotmail.com>
2020-01-20 22:38:45 +0000

    nested exercise child

---
+++ _Build/vue/components/VExercise/VExercise.vue
@@ -0,0 +1,74 @@
+<template>
+	<li>
+		<h3
+	    	v-text="exercise.title"
+	    />
+
+	    <form
+	    	class="
+	            grid
+	            grid--halfs
+	        "
+	        v-on:change="defaults"
+		>
+	        <div
+	        	v-for="(value, key) in exercise.defaults"
+	        >
+	            <label
+	            	v-text="key"
+	            />
+	            <input
+	            	type="number"
+	            	v-model.number="exercise.defaults[key]"
+	        	/>
+	        </div>
+	    </form>
+	    
+	    <ul class="
+	        grid
+	        grid--halfs
+	        grid--gutters
+	        grid--vertGutters
+	        ut-clearListOnly
+	    ">
+	        <li
+	        	v-for="(stat, key) in stats"
+	        >
+	            <div class="
+	                card
+	                card--workout
+	                ut-padding
+	                ut-noWrap
+	            ">
+	                <h3 v-text="key"/>
+	                <p v-text="stat"/>
+	            </div>
+	        </li>
+	    </ul>
+
+	    <form class="
+	        js-submit
+	        ut-textAlignRight
+	    ">
+	        <label
+            	v-text="'Weight'"
+            />
+            <input
+            	type="number"
+            	v-model.number="result.weight"
+        	/>
+
+        	<label
+            	v-text="'Reps'"
+            />
+            <input
+            	type="number"
+            	v-model.number="result.reps"
+        	/>
+
+	        <button type="submit" class="button">Set done</button>
+	    </form>
+	</li>
+</template>
+
+<script src="./VExercise.js"/>
--- _Build/vue/components/VWorkout/VWorkout.vue
+++ _Build/vue/components/VWorkout/VWorkout.vue
@@ -7,72 +7,15 @@
 		<template
 	    	v-for="(area, key) in exercises"
     	>       
-		    <li
+		    <VExercise
 		    	v-if="exercise.active"
 	        	v-for="exercise in area"
 	        	class="
 		        	ut-paddingHorz
 		    	"
-	        >
-		        <h3
-		        	v-text="exercise.title"
-		        />
-
-		        <form
-		        	class="
-		                grid
-		                grid--halfs
-		            "
-		            v-on:change="defaults(exercise, key)"
-		    	>
-	                <div
-	                	v-for="(value, key) in exercise.defaults"
-	                >
-	                    <label
-	                    	v-text="key"
-	                    />
-	                    <input
-	                    	type="number"
-	                    	v-model.number="exercise.defaults[key]"
-                    	/>
-	                </div>
-		        </form>
-		        
-		        <ul class="
-		            grid
-		            grid--halfs
-		            grid--gutters
-		            grid--vertGutters
-		            ut-clearListOnly
-		        ">
-	                <li
-	                	v-for="(stat, key) in stats(exercise)"
-	                >
-	                    <div class="
-	                        card
-	                        card--workout
-	                        ut-padding
-	                        ut-noWrap
-	                    ">
-	                        <h3 v-text="key"/>
-	                        <p v-text="stat"/>
-	                    </div>
-	                </li>
-		        </ul>
-
-		        <form class="
-		            js-submit
-		            ut-textAlignRight
-		        ">
-		            <label>Weight</label>
-		            <select class="js-actualWeight"></select>
-
-		            <label>Reps</label>
-		            <select class="js-actualReps"></select>
-
-		            <button type="submit" class="button">Set done</button>
-		        </form>
-		    </li>
+		    	:area="key"
+		    	:exercise="exercise"
+	        />
 		</template>
 	</ul>
 </template>
