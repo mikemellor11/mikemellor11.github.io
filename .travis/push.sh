06c93cb60eef71000875ce1272ba86b165b21dc3
mikemellor11 <mikemellor11@hotmail.com>
2020-05-24 15:24:51 +0100

    github token updates

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,6 +2,8 @@
 
 cd _Output && git add --all && git commit --allow-empty -m 'Deployed' && git reset --soft HEAD~2 && git commit -m 'Deployed'
 
+echo $token;
+
 git remote add origin-pages https://${token}@github.com/mikemellor11/mikemellor11.github.io.git > /dev/null 2>&1
 
 git push --quiet --set-upstream origin-pages master -f
